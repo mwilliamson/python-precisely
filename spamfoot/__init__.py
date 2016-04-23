ae5ef97ae9a448fd8df6591751383436691d7990
Michael Williamson <mike@zwobble.org>
2016-04-23 14:10:51 +0100

    Add newline to start of AssertionError

--- spamfoot/__init__.py
+++ spamfoot/__init__.py
@@ -5,4 +5,4 @@ from .object_matchers import has_property
 def assert_that(value, matcher):
     result = matcher.match(value)
     if not result.is_match:
-        raise AssertionError("Expected: {0}\nbut: {1}".format(matcher.describe(), result.explanation))
+        raise AssertionError("\nExpected: {0}\nbut: {1}".format(matcher.describe(), result.explanation))
