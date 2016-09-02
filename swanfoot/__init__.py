982018116faed1ffd544d8cf4d2af32b35ef4a9b
Michael Williamson <mike@zwobble.org>
2016-09-02 23:06:22 +0100

    Add has_feature matcher

--- swanfoot/__init__.py
+++ swanfoot/__init__.py
@@ -1,6 +1,7 @@
 from .core_matchers import equal_to, anything, all_of
 from .object_matchers import has_property, has_properties, instance_of
 from .iterable_matchers import contains_exactly, is_same_sequence
+from .feature_matchers import has_feature
 
 
 def assert_that(value, matcher):
---
+++ tests/has_feature_tests.py
@@ -0,0 +1,54 @@
+import collections
+
+from nose.tools import istest, assert_equal
+
+from swanfoot import has_feature, equal_to
+from swanfoot.results import matched, unmatched
+
+
+User = collections.namedtuple("User", ["username"])
+
+
+@istest
+def matches_when_feature_has_correct_value():
+    matcher = has_feature("name", lambda user: user.username, equal_to("bob"))
+    assert_equal(matched(), matcher.match(User("bob")))
+
+
+@istest
+def mismatches_when_feature_extraction_fails():
+    # TODO:
+    return
+    matcher = has_feature("name", lambda user: user.username, equal_to("bob"))
+    assert_equal(
+        unmatched(""),
+        matcher.match("bobbity")
+    )
+
+
+@istest
+def explanation_of_mismatch_contains_mismatch_of_feature():
+    matcher = has_feature("name", lambda user: user.username, equal_to("bob"))
+    assert_equal(
+        unmatched("name: was 'bobbity'"),
+        matcher.match(User("bobbity"))
+    )
+
+
+@istest
+def submatcher_is_coerced_to_matcher():
+    matcher = has_feature("name", lambda user: user.username, "bob")
+    assert_equal(
+        unmatched("name: was 'bobbity'"),
+        matcher.match(User("bobbity"))
+    )
+
+
+@istest
+def description_contains_description_of_property():
+    matcher = has_feature("name", lambda user: user.username, equal_to("bob"))
+    assert_equal(
+        "name: 'bob'",
+        matcher.describe()
+    )
+
