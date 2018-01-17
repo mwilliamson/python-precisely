a0b114836c7569169da1c43d97b22bcc7edc4bb7
Dan O'Donovan <dan.odonovan@gmail.com>
2018-01-17 15:07:10 +0000

    close_to matcher

--- precisely/comparison_matchers.py
+++ precisely/comparison_matchers.py
@@ -1,3 +1,4 @@
+from math import fabs
 import operator
 
 from .results import matched, unmatched
@@ -45,3 +46,22 @@ class ComparisonMatcher(object):
 
     def describe(self):
         return "{0} {1!r}".format(self._operator_description, self._value)
+
+
+def close_to(value, delta):
+    return IsCloseToMatcher(value, delta)
+
+
+class IsCloseToMatcher(object):
+    def __init__(self, value, delta):
+        self._value = value
+        self._delta = delta
+
+    def match(self, actual):
+        if fabs(actual - self._value) <= self._delta:
+            return matched()
+        else:
+            return unmatched("was {0!r}".format(actual))
+
+    def describe(self):
+        return "close to {0!r} +/- {1!r}".format(self._value, self._delta)
--- tests/numeric_comparison_tests.py
+++ tests/numeric_comparison_tests.py
@@ -1,6 +1,6 @@
 from nose.tools import istest, assert_equal
 
-from precisely import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to
+from precisely import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, close_to
 from precisely.results import matched, unmatched
 
 
@@ -58,3 +58,17 @@ def less_than_or_equal_to_matches_when_actual_is_less_than_or_equal_to_value():
 def less_than_or_equal_to_description_describes_value():
     matcher = less_than_or_equal_to(42)
     assert_equal("less than or equal to 42", matcher.describe())
+
+
+@istest
+def close_to_matches_when_actual_is_close_to_value_plus_delta():
+    matcher = close_to(42, 1)
+    assert_equal(matched(), matcher.match(43))
+    assert_equal(matched(), matcher.match(41))
+    assert_equal(unmatched("was 40"), matcher.match(40))
+
+
+@istest
+def close_to_description_describes_value():
+    matcher = close_to(42, 1)
+    assert_equal("close to 42 +/- 1", matcher.describe())
