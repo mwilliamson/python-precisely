1eb656506b9bc2ce18ff266e465bfe844b442403
Michael Williamson <mike@zwobble.org>
2019-04-20 15:13:52 +0100

    Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='precisely',
-    version='0.1.6',
+    version='0.1.7',
     description='Rich matchers, useful for assertions in tests. Inspired by Hamcrest.',
     long_description=read("README.rst"),
     author='Michael Williamson',
