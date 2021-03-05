0926df33db76458f530fd800619150ae3a338eab
Michael Williamson <mike@zwobble.org>
2021-03-05 23:00:31 +0000

    Reorder tests

--- tests/contains_exactly_tests.py
+++ tests/contains_exactly_tests.py
@@ -7,14 +7,14 @@ from precisely.results import matched, unmatched
 @istest
 def matches_when_all_submatchers_match_one_item_with_no_items_leftover():
     matcher = contains_exactly(equal_to("apple"), equal_to("banana"))
-    
+
     assert_equal(matched(), matcher.match(["banana", "apple"]))
 
 
 @istest
 def mismatches_when_actual_is_not_iterable():
     matcher = contains_exactly()
-    
+
     assert_equal(
         unmatched("was not iterable\nwas 0"),
         matcher.match(0)
@@ -24,10 +24,20 @@ def mismatches_when_actual_is_not_iterable():
 @istest
 def mismatches_when_item_is_missing():
     matcher = contains_exactly(equal_to("apple"), equal_to("banana"), equal_to("coconut"))
-    
+
     assert_equal(
         unmatched("was missing element:\n * 'banana'\nThese elements were in the iterable, but did not match the missing element:\n * 'coconut': was 'coconut'\n * 'apple': already matched"),
         matcher.match(["coconut", "apple"])
+    )
+
+
+@istest
+def mismatches_when_duplicate_is_missing():
+    matcher = contains_exactly(equal_to("apple"), equal_to("apple"))
+
+    assert_equal(
+        unmatched("was missing element:\n * 'apple'\nThese elements were in the iterable, but did not match the missing element:\n * 'apple': already matched"),
+        matcher.match(["apple"])
     )
 
 
@@ -52,19 +62,9 @@ def when_empty_iterable_is_expected_then_empty_iterable_matches():
 
 
 @istest
-def mismatches_when_duplicate_is_missing():
-    matcher = contains_exactly(equal_to("apple"), equal_to("apple"))
-    
-    assert_equal(
-        unmatched("was missing element:\n * 'apple'\nThese elements were in the iterable, but did not match the missing element:\n * 'apple': already matched"),
-        matcher.match(["apple"])
-    )
-
-
-@istest
 def mismatches_when_contains_extra_item():
     matcher = contains_exactly(equal_to("apple"))
-    
+
     assert_equal(
         unmatched("had extra elements:\n * 'coconut'"),
         matcher.match(["coconut", "apple"])
@@ -91,7 +91,7 @@ def description_uses_singular_when_there_is_one_submatcher():
 @istest
 def description_contains_descriptions_of_submatchers():
     matcher = contains_exactly(equal_to("apple"), equal_to("banana"))
-    
+
     assert_equal(
         "iterable containing these 2 elements in any order:\n * 'apple'\n * 'banana'",
         matcher.describe()
@@ -101,7 +101,7 @@ def description_contains_descriptions_of_submatchers():
 @istest
 def elements_are_coerced_to_matchers():
     matcher = contains_exactly("apple", "banana")
-    
+
     assert_equal(
         "iterable containing these 2 elements in any order:\n * 'apple'\n * 'banana'",
         matcher.describe()
